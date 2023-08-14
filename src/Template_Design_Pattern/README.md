d9d6baff4c92a78346b5efb26fc47534bef8855e
Ayush Abhinav <ayush.abhinav@bt.com>
2023-08-14 11:31:49 +0100

    template_design_pattern

---
+++ src/Template_Design_Pattern/README.md
@@ -0,0 +1,2 @@
+This pattern is usefull for removing the duplicate codes.
+Common steps are implemented in the base class and if needed overwritten by the child class.
---
+++ src/Template_Design_Pattern/query_template.py
@@ -0,0 +1,53 @@
+import datetime
+import sqlite3
+
+
+# Base class which server as the template
+class QueryTemplate:
+    def connect(self):
+        self.conn = sqlite3.connect(database="test.db")
+
+    def construct_query(self):
+        raise NotImplementedError()
+
+    def execute_query(self):
+        results = self.conn.execute(self.query)
+        self.result = results.fetchall()
+
+    def format_query(self):
+        pass
+        # put formatting code here
+
+    def output_result(self):
+        raise NotImplementedError()
+
+
+# Child class written to fit given purpose
+class NewVehicleQuery(QueryTemplate):
+    def construct_query(self):
+        self.query = """
+                        select 
+                            * 
+                        from sales 
+                        where new=true
+                    """
+
+    def output_result(self):
+        for result in self.results:
+            print(result)
+
+
+# Child class written to fit given purpose
+class UserGrossQuery(QueryTemplate):
+    def construct_query(self):
+        self.query = """
+                        select 
+                            sales_person, sum(sales_ammount) 
+                        from sales 
+                        group by sales_person
+                    """
+
+    def output_result(self):
+        file_name = f'gross_sales_{datetime.date.today().strftime("%Y%m%d")}'
+        with open(file_name, "w") as out_file:
+            out_file.write(self.result)
